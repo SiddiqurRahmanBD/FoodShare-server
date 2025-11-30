cb7172a371c1dbb8d2d4e4dee09b6d446846cbec
Siddiqur Rahman <mdsqkn@gmail.com>
2025-11-30 16:29:12 +0600

    Protect the User name Password

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+require("dotenv").config();
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 const app = express();
@@ -8,8 +9,7 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
-const uri =
-  "mongodb+srv://food:68mPPW7TD0xfU5UZ@duasmasi.bhtinpf.mongodb.net/?appName=Duasmasi";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@duasmasi.bhtinpf.mongodb.net/?appName=Duasmasi`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -80,6 +80,33 @@ async function run() {
       res.send(result);
     });
 
+    //Update Food API
+
+    app.put("/update-food/:id", async (req, res) => {
+      const id = req.params.id;
+      const data = req.body;
+      const query = { _id: new ObjectId(id) };
+      const UpdateFood = {
+        $set: data,
+      };
+      const result = await foodCollection.updateOne(query, UpdateFood);
+      res.send(result);
+    });
+
+    // Delete Food APi
+
+    app.delete("/delete-food", async (req, res) => {
+      const { id } = req.query;
+      const query = { _id: new ObjectId(id) };
+      const result = await foodCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+
     // request collection
 
     app.post("/request-food", async (req, res) => {
@@ -97,9 +124,8 @@ async function run() {
     app.patch("/accept-request/:id", async (req, res) => {
       try {
         const id = req.params.id;
-        const foodId = req.body.foodId; // ID of the food to update
+        const foodId = req.body.foodId;
 
-        // Update request status
         const requestQuery = { _id: new ObjectId(id) };
         const requestUpdate = { $set: { status: "accepted" } };
         const updatedRequest = await requestCollection.updateOne(
@@ -107,7 +133,7 @@ async function run() {
           requestUpdate
         );
 
-        //  Update food status → donated
+        //  Update food status
         const foodQuery = { _id: new ObjectId(foodId) };
         const foodUpdate = { $set: { status: "donated" } };
         const updatedFood = await foodCollection.updateOne(
@@ -122,6 +148,7 @@ async function run() {
       }
     });
 
+    // Rejected Api
     app.patch("/reject-request/:id", async (req, res) => {
       const id = req.params.id;
       const rejectQuery = { _id: new ObjectId(id) };
@@ -134,32 +161,16 @@ async function run() {
       res.send(result);
     });
 
-    //Update Food API
-
-    app.put("/update-food/:id", async (req, res) => {
-      const id = req.params.id;
-      const data = req.body;
-      const query = { _id: new ObjectId(id) };
-      const UpdateFood = {
-        $set: data,
-      };
-      const result = await foodCollection.updateOne(query, UpdateFood);
-      res.send(result);
-    });
-
-    // Deteel Food APi
-
-    app.delete("/delete-food", async (req, res) => {
-      const { id } = req.query;
-      const query = { _id: new ObjectId(id) };
-      const result = await foodCollection.deleteOne(query);
-      res.send(result);
-    });
-
-    await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
+    // Request My foods
+     app.get("/request-foods", async (req, res) => {
+       const email = req.query.email;
+       const result = await requestCollection
+         .find({
+           userEmail: email,
+         })
+         .toArray();
+       res.send(result);
+     });
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
